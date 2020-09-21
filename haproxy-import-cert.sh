fb39a5cb3b2632d268b4842f72755d1b7fdd695e
Stéphane Cottin <stephane.cottin@vixns.com>
2020-09-21 15:57:24 +0200

    typo

--- haproxy-import-cert.sh
+++ haproxy-import-cert.sh
@@ -5,12 +5,12 @@ HAPROXY_SOCKET="/var/run/haproxy/socket"
 [ -n "$HAPROXY_CERTS_DIR" ] || exit 1
 
 # Save cert to a file
-cat - >  "${HAPROXY_CERTS_DIR}${FQDN}.pem"
+cat - >  "${HAPROXY_CERTS_DIR}/${FQDN}.pem"
 
 # TODO: Why is this needed? Seems to active the socket or something
 echo "show info" | socat ${HAPROXY_SOCKET}  -
 
-echo "new ssl cert ${HAPROXY_CERTS_DIR}${FQDN}.pem" | socat ${HAPROXY_SOCKET} -
+echo "new ssl cert ${HAPROXY_CERTS_DIR}/${FQDN}.pem" | socat ${HAPROXY_SOCKET} -
 echo "show ssl cert" | socat ${HAPROXY_SOCKET} -
-echo -e "set ssl cert ${HAPROXY_CERTS_DIR}${FQDN}.pem <<\n$(cat ${HAPROXY_CERTS_DIR}${FQDN}.pem)\n" | socat ${HAPROXY_SOCKET} -
-echo "commit ssl cert ${HAPROXY_CERTS_DIR}${FQDN}.pem" | socat ${HAPROXY_SOCKET} -
+echo -e "set ssl cert ${HAPROXY_CERTS_DIR}/${FQDN}.pem <<\n$(cat ${HAPROXY_CERTS_DIR}/${FQDN}.pem)\n" | socat ${HAPROXY_SOCKET} -
+echo "commit ssl cert ${HAPROXY_CERTS_DIR}/${FQDN}.pem" | socat ${HAPROXY_SOCKET} -
